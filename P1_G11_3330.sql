581523a33b38ee4d3b1a71983ba0b75609c72bcb
tri2401 <tri2401na@gmail.com>
2022-02-14 15:12:44 -0600

    First Template SQL File

---
+++ P1_G11_3330.sql
@@ -0,0 +1,29 @@
+CREATE TABLE [Passenger]
+(
+
+);
+
+CREATE TABLE [Checks]
+(
+
+);
+
+CREATE TABLE [Train_Stat]
+(
+
+);
+
+CREATE TABLE [Train]
+(
+
+);
+
+CREATE TABLE [Train_Avail]
+(
+
+);
+
+CREATE TABLE [Train_Dest]
+(
+
+);
