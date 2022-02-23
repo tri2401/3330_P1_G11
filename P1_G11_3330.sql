a778f2f5877a458404b88865c82bd94e1601221d
shabieh50 <99703849+shabieh50@users.noreply.github.com>
2022-02-22 23:19:48 -0600

    Update P1_G11_3330.sql

--- P1_G11_3330.sql
+++ P1_G11_3330.sql
@@ -15,15 +15,26 @@ CREATE TABLE [Train_Stat]
 
 CREATE TABLE [Train]
 (
-	[TrainID] INTEGER NOT NULL
+	[TrainID] INTEGER NOT NULL,
+	[Name] VARCHAR(100) NOT NULL,
+	[Availablity] BOOL
+	[Source]
+	[Destination] NVARCHAR(50) NOT NULL
+	[TicketType] 
+	[TicketPrice] INTEGER
+	PRIMARY KEY (TrainID);
 );
 
 CREATE TABLE [Train_Avail]
 (
-
+	[TrainID] INTEGER NOT NULL,
+	[TrainAvail] BOOL NOT NULL,
+	PRIMARY KEY(TrainID, TrainAvail) 
+	FOREIGN KEY(TrainID) REFERENCES TRAIN(TrainID)
 );
 
 CREATE TABLE [Train_Dest]
 (
 
 );
+
